bc833a60538ccd695182e63df00c466d0c5fb163
aab92 <araujob.antonio@gmail.com>
2025-04-13 13:40:04 +0200

    async await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,7 +4,6 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req,res) => {
   //Write your code here
   const username = req.body.username;
@@ -20,43 +19,76 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.status(200).send(JSON.stringify(books, null, 4));
+public_users.get('/', async function (req, res) {
+  //return res.status(200).send(JSON.stringify(books, null, 4));
+  await getBooksAsync(req, res);
 });
 
+async function getBooksAsync(req, res) {
+  try {
+    return res.status(200).send(JSON.stringify(books, null, 4));
+  } catch (error) {
+    console.error("some error ocurred in getBooksAsync", error);
+  }
+}
+
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
+  await getBooksISBNAsync(req, res);
+});
+
+async function getBooksISBNAsync(req, res) {
+ try {
   const isbn = parseInt(req.params.isbn);
   if (isbn in books) {
     return res.status(200).send(JSON.stringify(books[isbn], null, 4));
   } else {
     return res.status(204).json({message: "isbn not found."});
   }
- });
+  } catch (error) {
+    console.error("some error ocurred in getBooksISBNAsync", error);
+  }
+}
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  let aubo = {}
-  const author = req.params.author;
-  for (let isbn in books) {
-    if (books[isbn].author === author) {
-      aubo[isbn] = books[isbn];
-    }
-  }
-  return res.status(200).send(JSON.stringify(aubo, null, 4));
+public_users.get('/author/:author', async function (req, res) {
+  await getBooksAuthorAsync(req, res);
 });
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  let titbo = {}
-  const title = req.params.title;
-  for (let isbn in books) {
-    if (books[isbn].title === title) {
-      titbo[isbn] = books[isbn];
+async function getBooksAuthorAsync(req, res) {
+  try {
+    let aubo = {}
+    const author = req.params.author;
+    for (let isbn in books) {
+      if (books[isbn].author === author) {
+        aubo[isbn] = books[isbn];
+      }
     }
+    return res.status(200).send(JSON.stringify(aubo, null, 4));
+  } catch (error) {
+    console.error("some error ocurred in getBooksAuthorAsync", error);
   }
-  return res.status(200).send(JSON.stringify(titbo, null, 4));
+}
+
+// Get all books based on title
+public_users.get('/title/:title', async function (req, res) {
+  await getBooksTitleAsync(req, res);
 });
+
+async function getBooksTitleAsync(req, res) {
+  try {
+    let titbo = {}
+    const title = req.params.title;
+    for (let isbn in books) {
+      if (books[isbn].title === title) {
+        titbo[isbn] = books[isbn];
+      }
+    }
+    return res.status(200).send(JSON.stringify(titbo, null, 4));
+  } catch (error) {
+    console.error("some error ocurred in getBooksTitleAsync", error);
+  }
+}
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
